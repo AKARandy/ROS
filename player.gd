db182fd750df7b5c6532398dd8700574c3b730cd
akmaldika <13521070@mahasiswa.itb.ac.id>
2024-01-13 23:11:03 +0700

    feat: lancarin movement

--- player.gd
+++ player.gd
@@ -4,21 +4,16 @@ const speed = 200
 var role
 
 func _physics_process(delta):
+	# Nolkan vektor kecepatan
+	velocity = Vector2()  
+
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_up"):
-		velocity.x = 0
-		velocity.y = -speed
-	elif Input.is_action_pressed("ui_down"):
-		velocity.x = 0
-		velocity.y = speed
-	else:
-		velocity.x = 0
-		velocity.y = 0
-	
+		velocity.x += speed
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= speed
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= speed
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += speed
+
 	move_and_slide()
-	
