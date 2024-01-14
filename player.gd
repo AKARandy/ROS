18e482552bedcb03f13809a5ad0bc6de6a1f645e
GustiInuKartapati <117675762+GustiInuKartapati@users.noreply.github.com>
2024-01-14 23:50:56 +0700

    nambah accel decel ama simplify line

--- player.gd
+++ player.gd
@@ -1,19 +1,28 @@
 extends CharacterBody2D
 
 const speed = 200
+const accel = 1500
+const friction = 600
+var input = Vector2.ZERO
 var role
 
 func _physics_process(delta):
-	# Nolkan vektor kecepatan
-	velocity = Vector2()  
+	player_movement(delta)
 
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += speed
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= speed
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= speed
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += speed
+func get_input():
+	input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	return input.normalized()
 
+func player_movement(delta):
+	input = get_input()
+	
+	if input == Vector2.ZERO:
+		if (velocity.length() > friction * delta):
+			velocity -= velocity.normalized() * (friction * delta)
+		else:
+			velocity = Vector2.ZERO 
+	else:
+		velocity += (input * accel * delta)
+		velocity = velocity.limit_length(speed)
+	
 	move_and_slide()
