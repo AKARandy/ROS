3ebe3bb69f02eb18ec4dfc476f65b652bdcae304
GustiInuKartapati <117675762+GustiInuKartapati@users.noreply.github.com>
2024-01-17 01:19:01 +0700

    Score System
    nambah score system dan destroy lawan pas collide

--- player.gd
+++ player.gd
@@ -4,7 +4,7 @@ const speed = 200
 const accel = 1500
 const friction = 600
 var input = Vector2.ZERO
-
+var score = 0
 
 @onready var _animated_sprite = $AnimatedSprite2D
 
@@ -12,18 +12,18 @@ func _physics_process(delta):
 	player_movement(delta)
 	if Global.role == "rock":
 		_animated_sprite.play("idle_rock")
-	if Global.role == "paper":
+	elif Global.role == "paper":
 		_animated_sprite.play("idle_paper")
-	if Global.role == "scissor":
+	elif Global.role == "scissor":
 		_animated_sprite.play("idle_scissor")
+
 func get_input():
 	input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	return input.normalized()
-	
 
 func player_movement(delta):
 	input = get_input()
-	
+
 	if input == Vector2.ZERO:
 		if (velocity.length() > friction * delta):
 			velocity -= velocity.normalized() * (friction * delta)
@@ -32,5 +32,23 @@ func player_movement(delta):
 	else:
 		velocity += (input * accel * delta)
 		velocity = velocity.limit_length(speed)
-	
+
 	move_and_slide()
+
+func increase_score():
+	score += 1
+	print("Score: ", score)
+
+func _on_area_2d_body_entered(body):
+	if Global.role == "paper":
+		if body.name == "ai_rock":
+			increase_score()
+			body.queue_free()
+	if Global.role == "rock":
+		if body.name == "ai_scissor":
+			increase_score()
+			body.queue_free()
+	if Global.role == "scissor":
+		if body.name == "ai_paper":
+			increase_score()
+			body.queue_free()
