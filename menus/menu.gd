1724412c06e4c75cc4239eda0adf1c0948aadfc3
akmaldika <13521070@mahasiswa.itb.ac.id>
2024-01-18 15:50:00 +0700

    fix minor bug lagi

--- menus/menu.gd
+++ menus/menu.gd
@@ -4,7 +4,7 @@ extends Control
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://menus/character_selection.gd")
+	get_tree().change_scene_to_file("res://menus/character_selection.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
