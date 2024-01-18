78af8ac9f2cb96188e894e91f31cec5fb73ca0da
akmaldika <13521070@mahasiswa.itb.ac.id>
2024-01-18 22:23:45 +0700

    fix bug

--- entities/enemy/ai_papper.gd
+++ entities/enemy/ai_papper.gd
@@ -7,19 +7,19 @@ var the_chased = null
 const CENTER = Vector2(450,300)
 enum {FLEEING, ROAMING}
 var state = ROAMING
-var body_to_flee_from : CharacterBody2D
+var body_to_flee_from : Vector2
 var random_target_pos : Vector2 = Vector2(450,300)
 
 func _physics_process(delta):
 	if chase == true:
-		var direction = (the_chased.position - self.position).normalized()
+		var direction = (the_chased - self.position).normalized()
 		velocity.x = direction.x * speed
 		velocity.y = direction.y * speed
 		
 	if chase == false:
 		match state:
 			FLEEING:
-				position -= position.direction_to(body_to_flee_from.position) * speed * delta
+				position -= position.direction_to(body_to_flee_from) * speed * delta
 			ROAMING:
 				if position.distance_to(random_target_pos) <= speed * delta:
 					random_target_pos = CENTER + Vector2(0,300).rotated(randf() * 2 * PI)
@@ -31,21 +31,21 @@ func _physics_process(delta):
 func _on_player_detection_body_entered(body):
 	if body.name == "player":
 		if EntityRoles.role == EntityRoles.Roles.ROCK:
-			the_chased = body
+			the_chased = body.position
 			chase = true
 		if EntityRoles.role == EntityRoles.Roles.SCISSOR:
 			chase = false
 			the_chased = null
 			state = FLEEING
-			body_to_flee_from = body
+			body_to_flee_from = body.position
 	if body.name == "ai_rock":
-		the_chased = body
+		the_chased = body.position
 		chase = true
 	if body.name == "ai_scissor":
 		chase = false
 		the_chased = null
 		state = FLEEING
-		body_to_flee_from = body
+		body_to_flee_from = body.position
 
 func _on_player_detection_body_exited(body):
 	if body.name == "player":
