efe9bbdf82b473b883023983792c94c396678036
AKARandy <mcpe490@gmail.com>
2024-05-18 12:38:55 +0700

    Added script to credits

---
+++ credits.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+
+
+func _on_return_pressed():
+	hide()
+
--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+@onready var credits = $CanvasLayer/credits
 
 
 func _on_play_pressed():
@@ -9,3 +9,8 @@ func _on_play_pressed():
 
 func _on_exit_pressed():
 	get_tree().quit()
+
+
+
+func _on_options_pressed():
+	credits.show()
